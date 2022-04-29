cfbcd631661fa694e4e78cc5bf982c06559b10fe
Andrew Ross <andrew.ross.mail@gmail.com>
2022-04-29 10:14:32 -0400

    Refactor src/nii_info.py
    - Add feature to enable choosing which sidecar/nii header fields
      are included in the output table
    - Add `-f/--fields` flag to the CLI

--- src/nii_info.py
+++ src/nii_info.py
@@ -2,11 +2,14 @@ from __future__ import annotations
 
 import argparse
 import csv
+import importlib
 import json
 import re
+from enum import Enum
 from io import TextIOWrapper
 from itertools import chain
 from pathlib import Path
+from typing import Any
 from typing import NamedTuple
 from typing import NoReturn
 from typing import Sequence
@@ -15,6 +18,35 @@ import nibabel as nib
 
 
 __version__ = "0.1.0"
+
+
+class FieldEntity(Enum):
+    SIDECAR = "sidecar"
+    HEADER = "header"
+    FUNCTION = "function"
+
+
+FIELD_ENTITIES = tuple(m.value for m in FieldEntity)
+SPEC_REGEX = re.compile(r"^(?P<entity>[^\:]+):(?P<name>[^\:]+)((?P<label>[^\:]*))?$")
+
+
+class NiftiField(NamedTuple):
+    entity: FieldEntity
+    name: str
+    label: str | None
+
+
+class Defaults:
+    fields = [
+        NiftiField(FieldEntity.SIDECAR, "ProtocolName", None),
+        NiftiField(FieldEntity.SIDECAR, "SeriesDescription", None),
+        NiftiField(FieldEntity.FUNCTION, "nii_info._get_dims", "shape"),
+        NiftiField(FieldEntity.FUNCTION, "nii_info._get_pixdims", "pixdims"),
+        NiftiField(FieldEntity.FUNCTION, "nii_info._elapsed_time", "elapsed Time (s)"),
+        NiftiField(FieldEntity.SIDECAR, "ImageType", None),
+        NiftiField(FieldEntity.FUNCTION, "nii_info._get_filename", "filename"),
+    ]
+    fields_alias = "[DEFAULTS]"
 
 
 def cli() -> NoReturn:
@@ -53,6 +85,13 @@ def create_parser(
         type=argparse.FileType("w"),
         help="file to write info to. (default: stdout)",
     )
+    parser.add_argument(
+        "-f",
+        "--fields",
+        default=["[DEFAULTS]"],
+        action="append",
+        help="Fields to include in the table.",
+    )
     parser.add_argument("-v", "--version", action="version", version=__version__)
     parser.add_argument(
         "-D",
@@ -69,12 +108,69 @@ def create_parser(
 
 def handler(ns: argparse.Namespace) -> int:
     paths: list[Path] = ns.path
-    out_tsv: TextIOWrapper = ns.out_tsv
+    tsvfile: TextIOWrapper = ns.out_tsv
+    specs = parse_fields_args(ns.fields)
+    fields = parse_field_specs(specs)
 
-    files = find_nii_files(paths)
-    write_tsv(files, out_tsv)
+    nii_info(paths, tsvfile, fields=fields)
 
     return 0
+
+
+def parse_fields_args(args: list[str]) -> list[str]:
+    specs: list[str] = []
+    for arg_s in args:
+        specs.extend(arg_s.split(","))
+    return specs
+
+
+def parse_field_specs(specs: list[str]) -> list[NiftiField]:
+    fields: list[NiftiField] = []
+    for spec in specs:
+        _spec = spec.strip()
+
+        # inject the default fields
+        if _spec == Defaults.fields_alias:
+            fields.extend(Defaults.fields)
+            continue
+
+        # parse the field spec
+        _match = SPEC_REGEX.match(_spec)
+        if _match is None:
+            raise SyntaxError(f"Failed to parse field spec: {_spec!r}")
+        groupdict = _match.groupdict()
+
+        # extract the parts from the parsed spec
+        entity_value = groupdict["entity"]
+        try:
+            entity = FieldEntity(entity_value)
+        except ValueError:
+            msg = (
+                f"Unexpected field type {entity_value!r}. "
+                f"Expected one of {FIELD_ENTITIES}"
+            )
+            raise ValueError(msg)
+        name = groupdict["name"]
+        label = groupdict["label"] or None
+
+        field = NiftiField(entity, name, label)
+        fields.append(field)
+
+    return fields
+
+
+def nii_info(
+    paths: list[str] | list[Path],
+    tsvfile: TextIOWrapper,
+    *,
+    fields: list[NiftiField] | None = None,
+):
+    fields = fields or Defaults.fields
+    files = find_nii_files(paths)
+    pairs = [load_pair(filename) for filename in files]
+    records = [create_record(img, sidecar, fields) for img, sidecar in pairs]
+    fieldnames = create_fieldnames(fields)
+    write_tsv(tsvfile, records, fieldnames)
 
 
 def find_nii_files(paths: list[str] | list[Path]) -> list[Path]:
@@ -88,48 +184,68 @@ def find_nii_files(paths: list[str] | list[Path]) -> list[Path]:
     return sorted(set(files))
 
 
-def write_tsv(files: list[str] | list[Path], tsvfile: TextIOWrapper):
-    fieldnames = ImgInfo._fields
-    writer = csv.DictWriter(tsvfile, fieldnames, delimiter="\t", lineterminator="\n")
-    writer.writeheader()
-
-    for filename in files:
-        info = ImgInfo.from_filename(filename)
-        writer.writerow(info._asdict())
+def load_pair(filename: str | Path) -> tuple[nib.Nifti1Image, dict[str, Any]]:
+    img = nib.load(filename)
+    sidecar_file = Path(re.sub(r"\.nii(\.gz)?$", ".json", str(filename)))
+    sidecar = json.loads(sidecar_file.read_text()) if sidecar_file.exists() else {}
+    return img, sidecar
 
 
-class ImgInfo(NamedTuple):
-    protocol_name: str
-    series_description: str
-    shape: tuple[int, int, int, int]
-    pixdims: tuple[float, float, float, float]
-    image_type: list[str]
-    seconds: float
-    filename: str
+def create_fieldnames(fields: list[NiftiField]) -> list[str]:
+    return [f.label or f.name for f in fields]
 
-    @classmethod
-    def from_filename(cls, filename: str | Path):
-        img = nib.load(filename)
-        sidecar_file = Path(re.sub(r"\.nii(\.gz)?$", ".json", str(filename)))
-        sidecar = json.loads(sidecar_file.read_text()) if sidecar_file.exists() else {}
 
-        protocol_name = sidecar.get("ProtocolName")
-        series_description = sidecar.get("SeriesDescription")
-        shape = tuple(img.header["dim"][1:5])
-        pixdims = tuple(img.header["pixdim"][1:5])
-        seconds = shape[3] * pixdims[3]
-        image_type = sidecar.get("ImageType")
-        filename = str(Path(filename).expanduser().resolve())
+def create_record(
+    img: nib.Nifti1Image, sidecar: dict[str, Any], fields: list[NiftiField]
+):
+    record: list[Any] = []
+    for field in fields:
+        if field.entity == FieldEntity.SIDECAR:
+            record.append(sidecar.get(field.name))
+        elif field.entity == FieldEntity.HEADER:
+            record.append(img.header[field.name])  # type: ignore
+        elif field.entity == FieldEntity.FUNCTION:
+            fn = fqn_import(field.name)
+            record.append(fn(img, sidecar))
+        else:
+            raise ValueError(f"Unexpected field type {field.entity!r}")
+    return record
 
-        return cls(
-            protocol_name,
-            series_description,
-            shape,
-            pixdims,
-            image_type,
-            seconds,
-            filename,
-        )
+
+def fqn_import(fqn: str):
+    module_name, _, function_name = fqn.rpartition(".")
+    return getattr(importlib.import_module(module_name), function_name)
+
+
+def write_tsv(tsvfile, records: list[Any], fieldnames: list[str]):
+    writer = csv.writer(tsvfile, delimiter="\t", lineterminator="\n")
+    writer.writerow(fieldnames)
+    writer.writerows(records)
+
+
+# --- UTILITY FUNCTIONS ---
+
+
+def _get_dims(
+    img: nib.Nifti1Image, sidecar: dict[str, Any]
+) -> tuple[int, int, int, int]:
+    return tuple(img.header["dim"][1:5])  # type: ignore
+
+
+def _get_pixdims(
+    img: nib.Nifti1Image, sidecar: dict[str, Any]
+) -> tuple[float, float, float, float]:
+    return tuple(img.header["pixdim"][1:5])  # type: ignore
+
+
+def _elapsed_time(img: nib.Nifti1Image, sidecar: dict[str, Any]) -> float:
+    shape = _get_dims(img, sidecar)
+    pixdims = _get_pixdims(img, sidecar)
+    return shape[-1] * pixdims[-1]
+
+
+def _get_filename(img: nib.Nifti1Image, sidecar: dict[str, Any]) -> str | None:
+    return img.get_filename()
 
 
 if __name__ == "__main__":
